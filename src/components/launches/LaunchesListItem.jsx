fd9866286faac13c661a5a6c7172a7db20f8fae6
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-03-01 18:03:59 +0100

    Added styled-component

--- src/components/launches/LaunchesListItem.jsx
+++ src/components/launches/LaunchesListItem.jsx
@@ -1,16 +1,15 @@
+import { Card } from "../../styledComponents/GlobalStyles";
 
 const LaunchesListItem = ({launch}) => {
     const {name, details} = launch;
     const {small} = launch.links.patch;
 
     return (
-        <>
-            <div className="launch-container">
-                <h3>Name: {name}</h3>
-                <img src={small} alt={launch.name} />
-                <p>{details}</p>
-            </div>
-        </>
+        <Card>
+            <h3>Name: {name}</h3>
+            <img src={small} alt={launch.name} />
+            <p>{details}</p>
+        </Card>
     )
 }
 
